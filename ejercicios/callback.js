14114daad7d82047191131dfc5e029a56d2e3055
Luisito <martinesj405@gmail.com>
2023-08-11 18:28:36 -0500

    Ejercicios callBack

---
+++ ejercicios/callback.js
@@ -0,0 +1,21 @@
+
+general();
+
+function general(){
+    console.log('1. funcion ');
+
+    bfuncion(cfuncion())
+
+
+
+}
+
+
+function bfuncion(dfuncion){
+    cfuncion(dfuncion);
+
+}
+
+function cfuncion(){
+    console.log('2. funcion')
+}
